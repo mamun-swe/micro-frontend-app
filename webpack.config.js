83b8783c629332eaf46214eb5d45f20477a4d30a
mamun-swe <mamun.swe.277@gmail.com>
2023-07-18 22:58:28 +0600

    public path changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3001/",
+    // publicPath: "http://localhost:3001/",
+    publicPath: "https://micro-frontend-app.vercel.app/",
   },
 
   resolve: {
